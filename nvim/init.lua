9bea3d398f185d4331aa63899e30d492c8d4b4e1
Kabir Jain <143137558+kabirdjain@users.noreply.github.com>
2026-05-14 17:23:43 -0400

    chore: fix typo

--- nvim/init.lua
+++ nvim/init.lua
@@ -38,6 +38,6 @@ vim.opt.mouse = 'a' --enable mouse
 
 if vim.loop.os_uname().sysname == "Linux" then
   vim.cmd.colorscheme("techbase")
-elseif vim.loop.os_name().sysname == "Dawin" then
+elseif vim.loop.os_uname().sysname == "Dawin" then
   vim.cmd.colorscheme("monokai-pro")
 end
